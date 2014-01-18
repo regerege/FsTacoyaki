4baca369cdd6bba00f92a17229b57df62fe290fb
regerege <regerege6@gmail.com>
2014-01-18 15:38:00 +0900

    Console Game for Tacoyaki

---
+++ FsTacoyaki/Program.fs
@@ -0,0 +1,122 @@
+﻿(*
+http://www.gamedesign.jp/flash/tacoyaki/tacoyaki.html
+5x5=25の裏と表のコインを同じ色に統一するゲーム
+コインをクリックすると斜め4方向に逆転する。
+
+●○○○○○●○○○○○●○○○○○●○○○○○●
+○○○○○●○○○○○●○○○○○●○●○○○●○
+○○○○○○○○○○●○○○●○●○●○○○●○○
+○○○○○○○○○●○○○●○●○●○○○●○○○
+○○○○●○○○●○○○●○○○●○○○●○○○○
+○●○○○○○●○○○○○●○○○○○●○○○●○
+●○○○○○●○○○○○●○●○○○●○○○●○●
+○○○○○●○○○●○●○●○○○●○○○●○●○
+○○○○●○○○●○●○●○○○●○○○●○●○○
+○○○●○○○●○○○●○○○●○○○○○●○○○
+○○●○○○○○●○○○○○●○○○●○○○●○○
+○●○○○○○●○●○○○●○○○●○●○●○○○
+●○○○●○●○●○○○●○○○●○●○●○○○●
+○○○●○●○●○○○●○○○●○●○○○○○●○
+○○●○○○●○○○●○○○○○●○○○○○●○○
+○○○●○○○○○●○○○●○○○●○○○●○○○
+○○●○●○○○●○○○●○●○●○○○●○○○○
+○●○●○○○●○○○●○●○●○○○●○○○○○
+●○●○○○●○○○●○●○○○○○●○○○○○●
+○●○○○●○○○○○●○○○○○●○○○○○●○
+○○○○●○○○●○○○●○○○●○○○●○○○○
+○○○●○○○●○●○●○○○●○○○○○○○○○
+○○●○○○●○●○●○○○●○○○○○○○○○○
+○●○○○●○●○○○○○●○○○○○●○○○○○
+●○○○○○●○○○○○●○○○○○●○○○○○●
+*)
+open System
+open System.Text.RegularExpressions
+
+let rec input() =
+    printf "xy: "
+    let t = Console.ReadLine()
+    if t.Length = 2 && Regex.IsMatch(t, "[1-5]{2}") then
+        1 <<< (int t.[0] - 49 + ((int t.[1] - 49) * 5))
+    else
+        printfn "Please enter twice the coordinates of 1~5."
+        input()
+
+type TacoyakiInput =
+    | None
+    | Position of int
+type TacoyakiConfig = {
+        Tacoyaki : int
+        Input : TacoyakiInput
+    }
+let mask =
+    [
+        0x1041041
+        0x00820a2
+        0x0004544
+        0x0008a88
+        0x0111110
+        0x0820822
+        0x1041445
+        0x008a88a
+        0x0115114
+        0x0222208
+        0x0410444
+        0x08288a8
+        0x1151151
+        0x02a2282
+        0x0444104
+        0x0208888
+        0x0511510
+        0x0a22a20
+        0x1445041
+        0x0882082
+        0x0111110
+        0x022a200
+        0x0454400
+        0x08a0820
+        0x1041041
+    ]
+let nzc (bit:int) =
+    if bit = 0 then 0
+    else
+        let rec find n c =
+            if n &&& 1 = 1 then c
+            else find (n >>> 1) (c+1)
+        find bit 0
+/// get reversal target
+let reverse (n:int) =
+    n |||
+    (n <<< 4) ||| (n <<< 6) ||| (n <<< 8) ||| (n <<< 12) ||| (n <<< 16) ||| (n <<< 18) ||| (n <<< 20) ||| (n <<< 24) |||
+    (n >>> 4) ||| (n >>> 6) ||| (n >>> 8) ||| (n >>> 12) ||| (n >>> 16) ||| (n >>> 18) ||| (n >>> 20) ||| (n >>> 24) &&& mask.[nzc n]
+let calc bit n =
+    let n = reverse n &&& 0x1FFFFFF
+    printfn "%A" n
+    bit ^^^ n
+    
+let inline display (bit:int) =
+    Console.Clear()
+    let coins = [| '○'; '●'; |]
+    let rec f m =
+        printf "%c" coins.[(bit >>> m &&& 0x00000001)]
+        if m % 5 = 0 then printfn ""
+        if 0 < m then f (m-1)
+    f 24
+
+let rec gameloop (config:TacoyakiConfig) =
+    let tacoyaki =
+        match config.Input with
+        | TacoyakiInput.Position n -> calc config.Tacoyaki n
+        | TacoyakiInput.None -> config.Tacoyaki
+    display tacoyaki
+    if tacoyaki = 0 || tacoyaki = 0x1FFFFFF then
+        printfn "Clear!"
+        Console.ReadKey() |> ignore
+    else
+        gameloop
+            { config with
+                Tacoyaki = tacoyaki
+                Input = TacoyakiInput.Position <| input()
+            }
+
+let rand = new System.Random()
+gameloop { Tacoyaki = rand.Next(1,24); Input = TacoyakiInput.None }
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# 関数型言語によるゲーム開発のサンプルコード
+
+題材：http://www.gamedesign.jp/flash/tacoyaki/tacoyaki.html
+開発言語: F# 2.0
+ライブラリ: .NET Framework 4.0
+
+<hr/>
+
+# Sample code of a functional programming for game development
+
+subject: http://www.gamedesign.jp/flash/tacoyaki/tacoyaki.html
+Language: F# 2.0
+Library: .NET Framework 4.0
